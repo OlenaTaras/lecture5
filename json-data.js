e96956d83bbe846321179ccb7d6d572c40c6eaaa
amds\olena.taras <olena.taras@advancedmd.com>
2019-03-14 15:26:26 +0200

    basic example

---
+++ json-data.js
@@ -0,0 +1,81 @@
+const usersData = Object.freeze({data: [
+  {
+    "index": 0,
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "eyeColor": "blue",
+    "name": "Mccray Levy",
+    "gender": "male",
+    "company": "ZILLA",
+    "email": "mccraylevy@zilla.com",
+    "phone": "+1 (826) 578-2781",
+    "address": "385 Hewes Street, Vicksburg, Colorado, 4207",
+    "about": "Ex labore proident aliquip cillum sunt. Ea adipisicing aliquip quis ad pariatur duis eu cillum. Deserunt est voluptate magna magna cillum officia labore. Laborum ipsum culpa ut officia aliquip sit non non.\r\n"
+  },
+  {
+    "index": 1,
+    "picture": "http://placehold.it/32x32",
+    "age": 34,
+    "eyeColor": "brown",
+    "name": "Jocelyn Mccullough",
+    "gender": "female",
+    "company": "XIXAN",
+    "email": "jocelynmccullough@xixan.com",
+    "phone": "+1 (982) 536-2968",
+    "address": "459 Furman Avenue, Chapin, Puerto Rico, 8969",
+    "about": "Deserunt veniam ullamco deserunt deserunt aliqua non officia fugiat exercitation Lorem sint velit occaecat non. Proident tempor sint dolore nulla ad duis deserunt laborum nisi aliquip exercitation. Id adipisicing aliqua enim voluptate culpa. Mollit ipsum ut voluptate occaecat cillum nostrud. Dolore labore fugiat nisi proident officia. Eu velit excepteur sunt deserunt laboris irure aliqua qui fugiat qui enim. Cillum magna proident eu incididunt qui voluptate tempor mollit tempor consequat laboris deserunt excepteur exercitation.\r\n"
+  },
+  {
+    "index": 2,
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "eyeColor": "brown",
+    "name": "Thomas Parsons",
+    "gender": "male",
+    "company": "ESCHOIR",
+    "email": "thomasparsons@eschoir.com",
+    "phone": "+1 (884) 560-3813",
+    "address": "505 Will Place, Limestone, Iowa, 6368",
+    "about": "Quis cillum adipisicing id exercitation velit ea officia cillum duis nostrud. Magna deserunt veniam irure ullamco laborum labore Lorem cupidatat fugiat culpa. Amet proident deserunt ullamco id commodo elit tempor laboris quis ad.\r\n"
+  },
+  {
+    "index": 3,
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "blue",
+    "name": "Cervantes Key",
+    "gender": "male",
+    "company": "ZILLANET",
+    "email": "cervanteskey@zillanet.com",
+    "phone": "+1 (978) 581-3514",
+    "address": "756 Nova Court, Goochland, New Jersey, 2207",
+    "about": "Aliqua exercitation ea cupidatat sint dolore. Exercitation fugiat sunt sit esse ipsum laborum eu. Ex anim ad dolor id ullamco non sunt nostrud velit ea sit culpa aliqua. Deserunt eiusmod velit magna labore do cillum dolore aliqua exercitation labore. Enim ut exercitation et quis esse nulla do. Quis Lorem occaecat minim ea ut esse sint excepteur.\r\n"
+  },
+  {
+    "index": 4,
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "green",
+    "name": "Sallie Gaines",
+    "gender": "female",
+    "company": "DEEPENDS",
+    "email": "salliegaines@deepends.com",
+    "phone": "+1 (913) 556-2783",
+    "address": "375 Forbell Street, Nelson, Tennessee, 7187",
+    "about": "Ad reprehenderit non et officia veniam eiusmod anim enim commodo id aute reprehenderit sint enim. Nostrud aliqua qui ex duis labore sunt deserunt elit dolore in sit aute duis. Nisi sint enim fugiat excepteur reprehenderit aute. Nulla commodo duis occaecat duis reprehenderit proident Lorem. Sit officia magna reprehenderit do deserunt proident. Duis dolore aliqua sunt pariatur anim. Laborum ea et excepteur enim do.\r\n"
+  },
+  {
+    "index": 5,
+    "picture": "http://placehold.it/32x32",
+    "age": 23,
+    "eyeColor": "brown",
+    "name": "Jayne Conrad",
+    "gender": "female",
+    "company": "EARTHWAX",
+    "email": "jayneconrad@earthwax.com",
+    "phone": "+1 (872) 440-2841",
+    "address": "639 Mayfair Drive, Reno, North Dakota, 8115",
+    "about": "Anim deserunt consectetur et ullamco. Tempor est qui esse laborum mollit velit exercitation nostrud amet. Commodo est aliqua qui magna elit amet ut aliqua. Culpa consectetur cillum in non ullamco culpa occaecat dolor velit.\r\n"
+  }
+]});
+
